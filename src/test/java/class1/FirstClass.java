97ca37587993580ff758f1315407cbf837172b41
Omar Faruk <omarfaruk.sust@gmail.com>
2024-05-27 02:25:22 -0400

    little change

--- src/test/java/class1/FirstClass.java
+++ src/test/java/class1/FirstClass.java
@@ -1,15 +1,22 @@
 package class1;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
 
 public class FirstClass {
     public static void main(String[] args) throws InterruptedException {
         WebDriver driver = new FirefoxDriver();
         driver.get("https://www.google.com");
+        System.out.println(driver.getTitle());
+        System.out.println(driver.getCurrentUrl());
 
         Thread.sleep(3000);
+        //driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
-        driver.quit();
+        //driver.close(); // This is to close the current tab only.
+        driver.quit(); // This will quit all the tabs opened by selenium
     }
 }
